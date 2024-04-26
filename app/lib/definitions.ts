dcbdfa31721110664d7d07ee7311f20e40d1a5f8
Abel François <abelitocoly@gmail.com>
2024-04-26 16:54:29 +0000

    Add files via upload

---
+++ app/lib/definitions.ts
@@ -0,0 +1,87 @@
+// Ce fichier contient les définitions de types pour vos données.
+// Il décrit la forme des données et le type de données que chaque propriété doit accepter.
+// Pour simplifier l'enseignement, nous définissons manuellement ces types.
+// Cependant, ces types sont générés automatiquement si vous utilisez un ORM tel que Prisma.
+export type User = {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+};
+
+export type Customer = {
+  id: string;
+  name: string;
+  email: string;
+  image_url: string;
+};
+
+export type Invoice = {
+  id: string;
+  customer_id: string;
+  amount: number;
+  date: string;
+// En TypeScript, cela s'appelle un type union de chaînes. Cela signifie que la propriété "status" ne peut être qu'une des deux chaînes: 'pending' ou 'paid'.
+  status: 'pending' | 'paid';
+};
+
+export type Revenue = {
+  month: string;
+  revenue: number;
+};
+
+export type LatestInvoice = {
+  id: string;
+  name: string;
+  image_url: string;
+  email: string;
+  amount: string;
+};
+
+// La base de données renvoie un nombre pour le montant, mais nous le formaterons plus tard en chaîne avec la fonction formatCurrency
+export type LatestInvoiceRaw = Omit<LatestInvoice, 'amount'> & {
+  amount: number;
+};
+
+export type InvoicesTable = {
+  id: string;
+  customer_id: string;
+  name: string;
+  email: string;
+  image_url: string;
+  date: string;
+  amount: number;
+  status: 'pending' | 'paid';
+};
+
+export type CustomersTableType = {
+  id: string;
+  name: string;
+  email: string;
+  image_url: string;
+  total_invoices: number;
+  total_pending: number;
+  total_paid: number;
+};
+
+export type FormattedCustomersTable = {
+  id: string;
+  name: string;
+  email: string;
+  image_url: string;
+  total_invoices: number;
+  total_pending: string;
+  total_paid: string;
+};
+
+export type CustomerField = {
+  id: string;
+  name: string;
+};
+
+export type InvoiceForm = {
+  id: string;
+  customer_id: string;
+  amount: number;
+  status: 'pending' | 'paid';
+};
