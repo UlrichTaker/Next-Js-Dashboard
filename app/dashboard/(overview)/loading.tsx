db700df83ea08c84c1319c5766b490ab6c5532ce
Abel François <abelitocoly@gmail.com>
2024-04-30 15:13:10 +0000

    Add files via upload

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,5 @@
+import DashboardSkeleton from '@/app/ui/skeletons';
+ 
+export default function Loading() {
+  return <DashboardSkeleton />; //Un squelette de chargement est une version simplifiée de l’interface utilisateur. De nombreux sites Web les utilisent comme espace réservé (ou solution de secours) pour indiquer aux utilisateurs que le contenu est en cours de chargement
+}
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -4,7 +4,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
-        <SideNav />
+        <SideNav />{/* Puisque <SideNav> est statique, il s'affiche immédiatement. L'utilisateur peut interagir avec <SideNav> pendant le chargement du contenu dynamique. */}
       </div>
       <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
     </div>
